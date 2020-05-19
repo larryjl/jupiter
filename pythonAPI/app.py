a49de6cc18616a4af3d866bcd6e46e589fbfe895
larryjl <leelawrencej@gmail.com>
2020-05-19 16:10:40 -0600

    feat: flask_cors and waitress for prod

--- pythonAPI/app.py
+++ pythonAPI/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from flask_restful import Api
 from flask_jwt import JWT
+from flask_cors import CORS
+from waitress import serve
 
 from security import authenticate, identity
 from resources.user import UserRegister
@@ -9,14 +11,18 @@ from resources.attempt import Attempt, AttemptList
 from db import db
 from postgres_config import pg_config
 
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     f'postgresql+psycopg2://{pg_config["user"]}:{pg_config["password"]}'
     f'@{pg_config["host"]}:{pg_config["port"]}/{pg_config["dbname"]}'
 )
+
 # disable flask_sqlalchemy tracker but keep sqlalchemy tracker
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 app.secret_key = "mykey"
+cors = CORS(app, resources={r"/*": {"origins": "http://localhost:5000"}})
 api = Api(app)
 
 
@@ -39,4 +45,5 @@ api.add_resource(Attempt, "/attempt/username=<string:username>")
 
 if __name__ == "__main__":
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    # app.run(port=5000, debug=True) # dev server
+    serve(app, host="127.0.0.1", port=5000) # prod WSGI server (waitress)
