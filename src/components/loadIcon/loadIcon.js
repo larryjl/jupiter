7814bdaf4d4a27a5ccaadd54be43e9b67de8da14
larryjl <leelawrencej@gmail.com>
2020-05-22 02:53:31 -0600

    feat: ellipsis dark option

--- src/components/loadIcon/loadIcon.js
+++ src/components/loadIcon/loadIcon.js
@@ -2,12 +2,10 @@ import React from "react";
 import { ReactComponent as EllipseSvg } from "../../images/icon_ellipsis.svg";
 import "./loadIcon.css";
 
-export default function LoadIcon (props) {
-  
+export default function LoadIcon(props) {
   return (
-    <span className="ellipsis">
-    <EllipseSvg 
-    />
+    <span className={`ellipsis ${props.theme ? props.theme : ""}`}>
+      <EllipseSvg />
     </span>
   );
 }
