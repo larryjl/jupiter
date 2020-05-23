ecb8aeae53d5d2374af429c9eb7ced5fd148a3b2
larryjl <leelawrencej@gmail.com>
2020-05-22 18:32:02 -0600

    feat: postgresql heroku environment url
    feat: postgresql heroku env url
    
    fix: chdir flaskAPI/
    
    fix: config .env key and urls
    
    fix: check heroku environment before .env

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,14 @@
 export const clientIds = {
-    google: '381072268579-0jo9s6uk126vi0hsc6rk52okmdvmkucm',
-}
+  google: "381072268579-0jo9s6uk126vi0hsc6rk52okmdvmkucm",
+};
 export const apiUrls = {
-    test: "https://localhost:44319/api",
-    development: "http://localhost:5000",
-    production: "https://graphhopperapi.azurewebsites.net/api"
-}
+  // -- dotnetAPI
+  // test: "https://localhost:44319/api",
+  // development: "https://localhost:44319/api",
+  // production: "https://graphhopperapi.azurewebsites.net/api"
+
+  // -- flaskAPI
+  test: "http://localhost:5000/api",
+  development: "http://localhost:5000/api",
+  production: "https://larryjl-jupiter.herokuapp.com/api",
+};
--- flaskAPI/app.py
+++ flaskAPI/app.py
@@ -1,35 +1,30 @@
+import os
+
 from flask import Flask, render_template
 from flask_restful import Api
 from flask_jwt import JWT
 from flask_cors import CORS
 
-from config import secret_key
 from security import authenticate, identity
 from resources.user import UserRegister, User
 from resources.attempt import UserAttempt, Attempt, AttemptList
 from resources.sequence import AttemptSequence, Sequence, SequenceList, UserSequence
 
 from db import db
-from postgres_config import pg_config
+
+if not os.environ.get("HEROKU"):
+    from .env import DATABASE_URL, SECRET_KEY
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f'postgresql+psycopg2://{pg_config["user"]}:{pg_config["password"]}'
-    f'@{pg_config["host"]}:{pg_config["port"]}/{pg_config["dbname"]}'
-)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", DATABASE_URL)
 
 # disable flask_sqlalchemy tracker but keep sqlalchemy tracker
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-app.secret_key = secret_key
+app.secret_key = os.environ.get("SECRET_KEY", SECRET_KEY)
 cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 api = Api(app)
-
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 @app.route("/")
@@ -39,15 +34,15 @@ def home():
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
-api.add_resource(UserRegister, "/register")
-api.add_resource(User, "/user/username=<string:username>")
-api.add_resource(UserAttempt, "/attempt/username=<string:username>")
-api.add_resource(Attempt, "/attempt/id=<string:attempt_id>")
-api.add_resource(AttemptList, "/attempts")
-api.add_resource(UserSequence, "/sequence/username=<string:username>")
-api.add_resource(AttemptSequence, "/sequence/attemptid=<string:attempt_id>")
-api.add_resource(Sequence, "/sequence/id=<string:sequence_id>")
-api.add_resource(SequenceList, "/sequences")
+api.add_resource(UserRegister, "/api/register")
+api.add_resource(User, "/api/users/username=<string:username>")
+api.add_resource(UserAttempt, "/api/attempts/username=<string:username>")
+api.add_resource(Attempt, "/api/attempts/id=<string:attempt_id>")
+api.add_resource(AttemptList, "/api/attempts")
+api.add_resource(UserSequence, "/api/sequences/username=<string:username>")
+api.add_resource(AttemptSequence, "/api/sequences/attemptid=<string:attempt_id>")
+api.add_resource(Sequence, "/api/sequences/id=<string:sequence_id>")
+api.add_resource(SequenceList, "/api/sequences")
 
 
 if __name__ == "__main__":
